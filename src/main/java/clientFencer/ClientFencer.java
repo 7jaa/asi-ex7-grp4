4d1350877b3edb8867a55f8e79dd5ba319182cd3
AlexandruOprean <opreanalexxii@yahoo.com>
2019-04-08 14:42:47 +0200

    First attempt to add FENCER - api which allows the creation of geographical areas and then check if given coordinates are in/out of the specific area. I have already created the three area as mentioned in class.

---
+++ src/main/java/clientFencer/ClientFencer.java
@@ -0,0 +1,42 @@
+package clientFencer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class ClientFencer {
+
+  URL url;
+
+  {
+    try {
+      url = new URL(
+          "https://api.fencer.io/v1.0");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      conn.setRequestProperty("Authorization","eaf5b589-6831-5945-897e-79c4872f4358");
+      conn.setRequestProperty("Accept", "application/json");
+      if (conn.getResponseCode() != 200) {
+        throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+      } else {
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+
+        String output;
+        while ((output = br.readLine()) != null) {
+          sb.append(output);
+        }
+      }
+    } catch (ProtocolException e) {
+      e.printStackTrace();
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
